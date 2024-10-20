45210dd93cbe181692608a9a8633a355428381c4
0200272128 <farbodm2004@gmail.com>
2024-10-20 10:43:30 +0330

    form

---
+++ store/useSurveyStore.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia';
+
+export const useSurveyStore = defineStore('surveyStore', {
+    state: () => ({
+        cartables: JSON.parse(localStorage.getItem('Cartables') || '[]'),
+        surveysList: JSON.parse(localStorage.getItem('SurveysList') || '[]'),
+        infoMonitored: '',
+    }),
+    getters: {
+        getCartableUserDataById: (state) => (id: number) => {
+            return state.cartables.find((item: any) => item.id === id);
+        },
+        getFilteredSurveys: (state) => (loanId: number) => {
+            return state.surveysList.filter((item: any) => item.id === loanId);
+        },
+    },
+    actions: {
+        loadFromLocalStorage() {
+            this.cartables = JSON.parse(localStorage.getItem('Cartables') || '[]');
+            this.surveysList = JSON.parse(localStorage.getItem('SurveysList') || '[]');
+        },
+    },
+});
