b8fe1798a1ce4a5ebd447ddb6ffca81b2dc812ac
0200272128 <farbodm2004@gmail.com>
2024-10-22 09:34:01 +0330

    form

--- layouts/survey.vue
+++ layouts/survey.vue
@@ -2,14 +2,12 @@
   <div id="app">
     <Loading/>
     <div id="main" class="mb-0 container bg-gray-1">
-<!--      <surveyHeader></surveyHeader>-->
       <slot />
     </div>
   </div>
 
 </template>
 <script setup>
-import surveyHeader from "~/components/Layouts/surveyHeader.vue";
 
 import {ToastNotificationService} from "~/core/toast-notification-service";
 import Loading from "~/components/loading/loading.vue";
